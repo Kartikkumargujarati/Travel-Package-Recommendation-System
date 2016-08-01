537c73aaf0d1bd1bf38278171b1135dd9f5b9975
Kartik Kumar Gujarati <kgujarati210@gmail.com>
2016-07-31 18:00:46 -0700

    Add files via upload

---
+++ src/java/pack/DbConnector.java
@@ -0,0 +1,37 @@
+package pack;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * @author KartikSalman
+ *
+ */
+public class DbConnector {
+    
+    public static Connection getConnection() {
+        Connection conn = null;
+        String url = "jdbc:mysql://localhost:3306/";
+        String dbName = "cocktail";
+        String driver = "com.mysql.jdbc.Driver";
+        String userName = "root";
+        String password = "root";
+        try {
+            Class.forName(driver).newInstance();
+            conn = DriverManager.getConnection(url + dbName, userName, password);
+            System.out.println("Connected to the database");
+    } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+     public static void main(String arg[]){
+        DbConnector.getConnection();
+    }
+}
